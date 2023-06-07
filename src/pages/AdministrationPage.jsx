aa742ccf865f7de74923e15713dfabed59a93315
Ramelito Martinez <ramelitomartinez@gmail.com>
2023-06-08 05:03:00 +0800

    made display of searched items work

--- src/pages/AdministrationPage.jsx
+++ src/pages/AdministrationPage.jsx
@@ -7,11 +7,24 @@ import Header from '../components/Header'
 
 const AdministrationPage=()=>{
     const [users,setUsers]=useState([]);
+    const [search,setSearch]=useState('');
+    const [count,setCount]=useState(0);
 
     const  fetchData =()=>{
+        if(search!=''){
+            return axios.get(`http://localhost:8008/api/v1/users/search/${search}`)
+            .then((response)=>{
+                setUsers(response.data)
+                setCount(response.data.length)
+            })
+            .catch(()=>{
+                console.log('data failed to load on search')
+            })
+        }
         return axios.get(`http://localhost:8008/api/v1/users`)
             .then((response)=>{
                 setUsers(response.data)
+                setCount(response.data.length)
             })
             .catch(()=>{
                 console.log('data failed to load')
@@ -20,15 +33,18 @@ const AdministrationPage=()=>{
 
     useEffect(()=>{
         fetchData();
-    },[])
-    
-    console.log('users',users)
-
+    },[search])
 
     return(
         <>
             <Header/>
             <h2>Users List</h2>
+            <label>Search: </label>
+            <input type='text'
+                placeholder='enter family name'
+                onChange = {(e)=>setSearch(e.target.value)}
+            />
+            <span> showing {count} results</span>
             <table>
                 <thead>
                     <tr>
@@ -41,21 +57,21 @@ const AdministrationPage=()=>{
                     </tr>
                 </thead>
                 <tbody>
-                        {
-                            users.map(user=>(
-                                <tr key={user._id}>
-                                    <td>{user._id}</td>
-                                    <td>{user.email}</td>
-                                    <td>{user.firstName}</td>
-                                    <td>{user.lastName}</td>
-                                    <td>{user.isAdmin}</td>
-                                    <td>
-                                        <button>Manage</button>
-                                        <button>Delete</button>
-                                    </td>
-                                </tr>
-                            ))
-                        }
+                    {
+                        users.map(user=>(
+                            <tr key={user._id}>
+                                <td>{user._id}</td>
+                                <td>{user.email}</td>
+                                <td>{user.firstName}</td>
+                                <td>{user.lastName}</td>
+                                <td>{user.isAdmin}</td>
+                                <td>
+                                    <button>Manage</button>
+                                    <button>Delete</button>
+                                </td>
+                            </tr>
+                        ))
+                    }
                 </tbody>
             </table>
         </>
