f8335f6812153e4306e91f8dbc09d2e8f8502151
ramlittle <ramelitomartinez@gmail.com>
2023-07-04 17:32:09 +0800

    feat: fix uploading of image

--- src/tests/fileUpload/FileUpload.jsx
+++ src/tests/fileUpload/FileUpload.jsx
@@ -1,41 +1,44 @@
-import {useState} from 'react'
+import { useState } from 'react'
 import axios from 'axios';
-const FileUpload=()=>{
-    const [image,setImage]=useState('')
+const FileUpload = () => {
+    const [image, setImage] = useState('')
 
-    const handleSubmit=(e)=>{
+    const handleSubmit = (e) => {
         e.preventDefault();
-        console.log('selected image',image)
+        console.log('selected image', image)
 
         const configuration = {
-                    method: 'post',
-                    url: 'http://localhost:8008/api/v1/image/upload',
-                    data: {
-                        image:image,
-                    },
-                  };
-                
-                  // make the API call
-                  axios(configuration)
-                    // .then((result) => {
-                    //   if(result.data.status=='User already exists'){
-                    //     setErrorMessage('User already exists');
-                    //   }else{
-                    //     alert(result.data.status);
-                    //     navigate('/LoginPage');
-                    //   }
-                      
-                    // })
-                    .catch((error) => {
-                      alert(error.response.data.status);
-                    }); 
+            method: 'post',
+            headers: {
+                'Content-Type': 'multipart/form-data'
+              },
+            url: 'http://localhost:8008/api/v1/image/upload',
+            data: {
+                file: image,
+            },
+        };
+
+        // make the API call
+        axios(configuration)
+            // .then((result) => {
+            //   if(result.data.status=='User already exists'){
+            //     setErrorMessage('User already exists');
+            //   }else{
+            //     alert(result.data.status);
+            //     navigate('/LoginPage');
+            //   }
+
+            // })
+            .catch((error) => {
+                alert(error.response.data.status);
+            });
     }
-    return(
+    return (
         <>
-            <form onSubmit={(e)=>handleSubmit(e)} enctype='multipart/form-data'>
+            <form onSubmit={(e) => handleSubmit(e)} encType='multipart/form-data'>
                 <label>Image</label>
                 <input type='file' name='image'
-                    onChange={(e)=>setImage(e.target.files[0])}
+                    onChange={(e) => setImage(e.target.files[0])}
                 />
                 <button>Submit</button>
             </form>
