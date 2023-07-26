2e4b8303e5bbdbd4fb185a53615c20c7408e26df
ramelitomartinez <ramelitomartinez@gmail.com>
2023-07-25 22:00:07 -0400

    feat: made display of after deductions have margins

--- src/tests/ImportExcel/ExcelImporter.jsx
+++ src/tests/ImportExcel/ExcelImporter.jsx
@@ -1,13 +1,13 @@
 
 import { useState, useEffect } from 'react'
 import * as XLSX from 'xlsx'
-import { addTitle,insertAfterSpecificKey,knowDivider } from './functions.js'
+import { addTitle, insertAfterSpecificKey, knowDivider } from './functions.js'
 
 const ExcelImporter = () => {
 
     const [excelData, setExcelData] = useState(null);
     const [payListTitle, setPaylistTitle] = useState('')
-    const [divider,setDivider]=useState(0);
+    const [divider, setDivider] = useState(0);
     const handleFileChange = (e) => {
         const file = e.target.files[0];
         if (!file) return;
@@ -30,44 +30,44 @@ const ExcelImporter = () => {
             const newValue = "";
             const updatedArray = insertAfterSpecificKey(jsonData);
             console.log(updatedArray);
-            knowDivider(updatedArray)
+            setDivider(knowDivider(updatedArray));
+            console.log('heres the official divider', divider)
             setExcelData(addTitle(updatedArray, payListTitle));
-            
         };
 
         reader.readAsArrayBuffer(file);
     };
 
-    useEffect(()=>{
-        console.log('here is now your new excel data',excelData);
-    },[excelData]);
+    useEffect(() => {
+        console.log('here is now your new excel data', excelData);
+    }, [excelData]);
 
-     return (
+    return (
         <>
             <div>
-                <input 
+                <input
                     className='p-1 m-1'
-                    type='text' value={payListTitle} 
-                    onChange={(e) => { setPaylistTitle(e.target.value) }} 
+                    type='text' value={payListTitle}
+                    onChange={(e) => { setPaylistTitle(e.target.value) }}
                     placeholder='Title - ex: May 1-15, 2023'
                 />
             </div>
             <div>
                 {
                     payListTitle &&
-                    <input 
+                    <input
                         className='p-1'
-                        type="file" 
-                        onChange={handleFileChange} 
-                        />
+                        type="file"
+                        onChange={handleFileChange}
+                    />
                 }
             </div>
-            {excelData && (
-        <div>
-          <h2>Excel Data:</h2>
-          <pre>{JSON.stringify(excelData, null, 1)}</pre>
-        </div>
-      )}
+            {/* {excelData && (
+                <div>
+                    <h2>Excel Data:</h2>
+                    <pre>{JSON.stringify(excelData, null, 1)}</pre>
+                </div>
+            )} */}
             <div className='flex flex-wrap border border-red-500'>
                 {
                     excelData &&
@@ -81,39 +81,47 @@ const ExcelImporter = () => {
                             <div className=''>
                                 <table className='ring-2 w-full text-xs'>
                                     <tbody>
-                                    {Object.entries(obj).map(([key, value],index) => (
-                                        <tr key={key}>
-                                            {/* {console.log('index',index)} */}
-                                            {
-                                             key ==='Net Amount' || key ==='Employee Name' || key === 'Basic Salary'? 
-                                             <>
-                                                <td><strong>{key}</strong></td>
-                                                <td ><strong className='ml-10'><u>{value}</u></strong></td>
-                                            </>
-                                            :
-                                                key ==='Total Earned' ? 
-                                                <>
-                                                    <td>{key}</td>
-                                                    <td ><strong className='ml-5 overline'><u>{value}</u></strong></td>
-                                                    
-                                                </>
-                                                :
-                                                    key ==='Total Deductions' ?
-                                                    <>
-                                                        <td>{key}</td>
-                                                        <td ><strong className='ml-5 overline'><u>{value}</u></strong></td>
-                                                    </>
+                                        {Object.entries(obj).map(([key, value], index) => (
+                                            <tr key={key}>
+                                                {/* {console.log('index',index)} */}
+                                                {
+                                                    key === 'Net Amount' || key === 'Employee Name' || key === 'Basic Salary' ?
+                                                        <>
+                                                            <td><strong>{key}</strong></td>
+                                                            <td ><strong className='ml-10'><u>{value}</u></strong></td>
+                                                        </>
                                                         :
-                                                            key ==='title'?
-                                                            null
-                                                            :
+                                                        key === 'Total Earned' ?
                                                             <>
-                                                                <td className=''>{key}</td>
-                                                                <td><span className='ml-5'>{value} </span></td>
+                                                                <td>{key}</td>
+                                                                <td ><strong className='ml-5 overline'><u>{value}</u></strong></td>
+
                                                             </>
-                                            }
-                                        </tr>
-                                    ))}
+                                                            :
+                                                            key === 'Total Deductions' ?
+                                                                <>
+                                                                    <td>{key}</td>
+                                                                    <td ><strong className='ml-5 overline'><u>{value}</u></strong></td>
+                                                                </>
+                                                                :
+                                                                key === 'title' ?
+                                                                    null
+                                                                    :
+
+                                                                    index > divider ?
+                                                                        <>
+                                                                            {console.log('dividier here', divider, 'index here', index)}
+                                                                            <td ><span className='ml-5'>{key}</span></td>
+                                                                            <td><span className='ml-5'>{value} </span></td>
+                                                                        </>
+                                                                        :
+                                                                        <>
+                                                                            <td className=''>{key}</td>
+                                                                            <td><span className='ml-5'>{value} </span></td>
+                                                                        </>
+                                                }
+                                            </tr>
+                                        ))}
                                     </tbody>
                                 </table>
                             </div>
